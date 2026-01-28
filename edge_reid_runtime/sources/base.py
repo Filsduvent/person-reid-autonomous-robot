ff6fa6f0ecf0fe1ca86b580d15f1b2c86c545fa8
filsduvent <dondalbrownirakoz@gmail.com>
2026-01-28 09:45:42 -0300

    Unify BaseSource with runtime InputSource

---
+++ edge_reid_runtime/sources/base.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from edge_reid_runtime.core.interfaces import Frame, InputSource
+
+
+class BaseSource(InputSource):
+    """Alias for runtime input sources."""
